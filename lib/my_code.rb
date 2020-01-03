bf3cc5dcbb9518c6a72a0d8bc0a47d7f48797700
Adit P Patel <app2116@gmail.com>
2020-01-03 21:31:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,9 +31,8 @@ return source_array
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-    p starting_point
-    i=starting_point
-    total = 0
+    i=0
+    total = starting_point
     pp source_array
   while source_array[i] do
     total += source_array[i]
