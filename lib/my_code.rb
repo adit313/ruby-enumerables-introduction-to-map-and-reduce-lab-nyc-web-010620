556939cd81b6771d6764d8156b925ce1026d082a
Adit P Patel <app2116@gmail.com>
2020-01-03 21:24:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # My Code here....
+def map_to_negativize(source_array)
+  i=0 
+  while source_array[i] do
+    source_array[i] *= -1
+    i += 1
+end
+return source_array
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+    i=0 
+  while source_array[i] do
+    source_array[i] *= -1
+    i += 1
+end
+return source_array
+end
