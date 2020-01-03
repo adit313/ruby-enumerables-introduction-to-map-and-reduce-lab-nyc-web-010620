243d92c590915eb4845fd9d6225078ecadb39f95
Adit P Patel <app2116@gmail.com>
2020-01-03 21:34:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,7 +44,7 @@ end
 def reduce_to_all_true(source_array)
     i=0
   while source_array[i] do
-    return false if source_array[i] == false
+    return false unless source_array[i]
     i += 1
 end
 return true
@@ -53,7 +53,7 @@ end
 def reduce_to_any_true(source_array)
     i=0
   while source_array[i] do
-    return true if source_array[i] == true
+    return true unless source_array[i]
     i += 1
 end
 return false
